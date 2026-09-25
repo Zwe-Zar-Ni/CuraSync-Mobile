3f11f55d192bca520a39a27ee44acaccd89d2d7d
Zwe Zar Ni <heinzwe2626@gmail.com>
2026-09-25 21:10:19 +0630

    Clean up + Packages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+import "../global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Text, View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <View>
+      <Text className="text-3xl">Hello world</Text>
+    </View>
   );
 }
